73815399938ff45abf117760c03706106fb73fb3
SergeyDedikov <serded59@yandex.ru>
2023-03-06 16:05:37 +0500

    feat: chapter 6_01 task 4

---
+++ 06_Advanced_work_with_functions/06_01_Recursion_and_Stack/task04.js
@@ -0,0 +1,66 @@
+/* 
+Задача 4
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+Напишите функцию printList(list), которая выводит элементы списка по одному.
+
+Сделайте два варианта решения: используя цикл и через рекурсию.
+
+Как лучше: с рекурсией или без?
+*/
+
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+// 1) через цикл
+
+function printList(list) {
+  let tmp = list;
+
+  while (tmp) {
+    console.log(tmp.value);
+    tmp = tmp.next;
+  }
+
+}
+
+printList(list);
+
+
+// 2) через рекурсию
+
+function printListRec(list) {
+
+  console.log(list.value);
+
+  if (list.next) {
+    printListRec(list.next);
+  }
+
+}
+
+printList(list);
